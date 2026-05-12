91fc634595880d7343a385098b68c04b6402ac65
MOHAMMED-BOUDIA <mohamedboudia516@gmail.com>
2026-05-12 01:34:25 +0100

    Refactor code structure for improved readability and maintainability

--- src/data/projects.js
+++ src/data/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: 'E-Commerce Platform',
     description:
       'A full-stack online store with secure authentication, catalog browsing, cart persistence, and Stripe-ready checkout flow.',
-    image: '/public/images/ecompicc.jpg',
+    image: '/img/ecompicc.jpg',
     stack: ['React', 'Tailwind CSS', 'Node.js', 'Express.js', 'MongoDB'],
     githubLink: 'https://github.com/MOHAMMED-BOUDIA/ecommerce-v2',
     live: 'https://vanguard-sepia.vercel.app/',
@@ -16,7 +16,7 @@ export const projects = [
     title: 'DEVIS FACTURATION Solution pour PME',
     description:
       'A smart invoicing and business management solution tailored for small and medium-sized enterprises (SMEs).',
-    image: '/public/images/DEVIS-FACTURATION-Solution-pour-PME-.jpg',
+    image: '/img/DEVIS-FACTURATION-Solution-pour-PME-.jpg',
     stack: ['React', 'Node.js', 'Express.js', 'MongoDB', 'Socket.IO'],
     githubLink: 'https://github.com/MOHAMMED-BOUDIA/DEVIS-FACTURATION-Solution-pour-PME-',
     live: 'https://devis-facturation-solution-pour-pme-gamma.vercel.app/',
@@ -28,7 +28,7 @@ export const projects = [
     title: 'CRM',
     description:
       'modern Customer Relationship Management (CRM) system built to centralize customer data, streamline communication, and enhance sales performance..',
-    image: '/public/images/crmpic.jpg',
+    image: '/img/crmpic.jpg',
     stack: ['React', 'Vite', 'Tailwind CSS', 'Netlify'],
     githubLink: 'https://github.com/MOHAMMED-BOUDIA/crm',
     live: 'https://crm-orpin-zeta.vercel.app/',
